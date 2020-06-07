1d2b7a795f480fa9ed3a74dc326b0d1c8c41be49
Tom Witkowski <dev.gummibeer@gmail.com>
2020-06-07 03:37:58 +0200

    add missing types to OG

--- src/OpenGraph.php
+++ src/OpenGraph.php
@@ -4,7 +4,15 @@ namespace Astrotomic\OpenGraph;
 
 use Astrotomic\OpenGraph\Types\Article;
 use Astrotomic\OpenGraph\Types\Book;
+use Astrotomic\OpenGraph\Types\Music\Album;
+use Astrotomic\OpenGraph\Types\Music\Playlist;
+use Astrotomic\OpenGraph\Types\Music\RadioStation;
+use Astrotomic\OpenGraph\Types\Music\Song;
 use Astrotomic\OpenGraph\Types\Profile;
+use Astrotomic\OpenGraph\Types\Video\Episode;
+use Astrotomic\OpenGraph\Types\Video\Movie;
+use Astrotomic\OpenGraph\Types\Video\Other;
+use Astrotomic\OpenGraph\Types\Video\TvShow;
 use Astrotomic\OpenGraph\Types\Website;
 
 class OpenGraph
@@ -27,5 +35,45 @@ class OpenGraph
     public static function profile(?string $title = null): Profile
     {
         return Profile::make($title);
+    }
+
+    public static function movie(?string $title = null): Movie
+    {
+        return Movie::make($title);
+    }
+
+    public static function tvShow(?string $title = null): TvShow
+    {
+        return TvShow::make($title);
+    }
+
+    public static function episode(?string $title = null): Episode
+    {
+        return Episode::make($title);
+    }
+
+    public static function other(?string $title = null): Other
+    {
+        return Other::make($title);
+    }
+
+    public static function album(?string $title = null): Album
+    {
+        return Album::make($title);
+    }
+
+    public static function song(?string $title = null): Song
+    {
+        return Song::make($title);
+    }
+
+    public static function playlist(?string $title = null): Playlist
+    {
+        return Playlist::make($title);
+    }
+
+    public static function radioStation(?string $title = null): RadioStation
+    {
+        return RadioStation::make($title);
     }
 }
