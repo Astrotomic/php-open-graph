231a053b6bc2e3b9a19999f845d970ad040f89ea
Tom Witkowski <dev.gummibeer@gmail.com>
2020-06-09 16:10:47 +0200

    add missing creator

--- src/Types/Twitter/SummaryLargeImage.php
+++ src/Types/Twitter/SummaryLargeImage.php
@@ -7,4 +7,11 @@ use Astrotomic\OpenGraph\TwitterType;
 class SummaryLargeImage extends TwitterType
 {
     protected string $type = 'summary_large_image';
+
+    public function creator(string $creator)
+    {
+        $this->setProperty(self::PREFIX, 'creator', $creator);
+
+        return $this;
+    }
 }
